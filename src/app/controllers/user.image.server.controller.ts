76f09736da9fd355db1f14ba9d33769d3c5991c5
Jed <jat200@uclive.ac.nz>
2023-03-28 10:51:42 +1300

    Started on implementing users images

--- src/app/controllers/user.image.server.controller.ts
+++ src/app/controllers/user.image.server.controller.ts
@@ -1,12 +1,26 @@
 import {Request, Response} from "express";
 import Logger from "../../config/logger";
-
+import * as usersImage from '../models/user.image.server.model';
+import fs from 'mz/fs';
+import {findUserIdByToken} from "../models/user.server.model";
 
 const getImage = async (req: Request, res: Response): Promise<void> => {
     try{
-        // Your code goes here
-        res.statusMessage = "Not Implemented Yet!";
-        res.status(501).send();
+        Logger.http(`GET single user id: ${req.params.id}`);
+        if (isNaN(parseInt(req.params.id, 10))) {
+            res.status(404).send("Not Found. No user with specified ID");
+            return;
+        }
+        const id = req.params.id;
+        const result = await usersImage.getOne(parseInt(id, 10));
+        if (result.length !== 0) {
+            const content = fs.readFile(result[0].image_filename);
+            res.status(200).send(content);
+        } else {
+            res.statusMessage = "Not Found. No user with specified ID, or user has no image";
+            res.status(404).send();
+        }
+
         return;
     } catch (err) {
         Logger.error(err);
@@ -19,7 +33,22 @@ const getImage = async (req: Request, res: Response): Promise<void> => {
 
 const setImage = async (req: Request, res: Response): Promise<void> => {
     try{
-        // Your code goes here
+        const id = req.params.id;
+        const token = req.header('X-Authorization');
+        const authUserId = token != null ? await findUserIdByToken(token) : null;
+        if (authUserId === null) {
+            res.statusMessage = 'Unauthorized';
+            res.status(401).send();
+            return;
+        }
+        if (authUserId !== parseInt(id, 10)) {
+            res.statusMessage = 'Forbidden. Can not change another user\'s profile photo';
+            res.status(403).send();
+            return;
+        }
+        const contentType = req.header('Content-Type');
+        // Add file
+        const result = await usersImage.insert(parseInt(id, 10), req.body);
         res.statusMessage = "Not Implemented Yet!";
         res.status(501).send();
         return;
---
+++ src/app/models/user.image.server.model.ts
@@ -0,0 +1,23 @@
+import {getPool} from '../../config/db';
+import Logger from '../../config/logger';
+import {ResultSetHeader} from 'mysql2';
+
+const getOne = async (id: number): Promise<{"image_filename": string}[]> => {
+    Logger.info(`Getting user ${id} profile image from database`);
+    const conn = await getPool().getConnection();
+    const query = `select image_filename from user where id = ${id}`;
+    const [ result ] = await conn.query(query);
+    await conn.release();
+    return result;
+}
+
+const insert = async (id: number, filename: string): Promise<ResultSetHeader[]> => {
+    Logger.info(`Adding image to database`);
+    const conn = await getPool().getConnection();
+    const query = `insert into users (image_filename) '${filename}' where id = ${id}`;
+    const [ result ] = await conn.query(query);
+    await conn.release();
+    return result;
+}
+
+export { getOne, insert }
